2303074725e402ab71b55bf95c4c448983a7d988
Luke Shingles <luke.shingles@gmail.com>
2020-09-29 10:37:44 +0100

    Change SECRET_KEY and hide from repo

--- atlasserver/atlasserver/settings.py
+++ atlasserver/atlasserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from atlasserver.settings_secret import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eap4gxw@_hkco-@%ei&pmn321e=y9%!y*d6+-onhfrdegt5+h7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
