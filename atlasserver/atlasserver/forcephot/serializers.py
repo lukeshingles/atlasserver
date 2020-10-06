1912268a7068eb984498c4bccfde674afd57ec8c
Luke Shingles <luke.shingles@gmail.com>
2020-10-06 13:08:18 +0100

    Only show result URL if job is finished

--- atlasserver/atlasserver/forcephot/serializers.py
+++ atlasserver/atlasserver/forcephot/serializers.py
@@ -20,8 +20,15 @@ class ForcePhotTaskSerializer(serializers.ModelSerializer):
     result_url = serializers.SerializerMethodField('get_result_url')
 
     def get_result_url(self, obj):
-        request = self.context.get('request')
-        return request.build_absolute_uri(f'/static/results/job{int(obj.id):05d}.txt')
+        localresultfile = f'/static/results/job{int(obj.id):05d}.txt'
+        # TODO: what if it's finished but the file doesn't exist or vice-versa?
+        # if not os.path.exists(localresultfile):
+
+        if obj.finished:
+            request = self.context.get('request')
+            return request.build_absolute_uri(localresultfile)
+
+        return None
 
     class Meta:
         model = Tasks
