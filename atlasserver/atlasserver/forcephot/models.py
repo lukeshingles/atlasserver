3573f0d395613e2561d7194fa4abf3521e9fa55d
Luke Shingles <luke.shingles@gmail.com>
2020-10-09 13:59:52 +0100

    Use DecimalField for ra and dec

--- atlasserver/atlasserver/forcephot/models.py
+++ atlasserver/atlasserver/forcephot/models.py
@@ -3,14 +3,20 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 
+class RoundingDecimalField(models.DecimalField):
+    def validate_precision(self, value):
+        return value
+
+
+
 class Tasks(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    ra = models.FloatField()
-    dec = models.FloatField()
+    ra = models.DecimalField(max_digits=8, decimal_places=5)
+    dec = models.DecimalField(max_digits=8, decimal_places=5)
     mjd_min = models.FloatField(null=True, blank=True, default=None)
     mjd_max = models.FloatField(null=True, blank=True, default=None)
     use_reduced = models.BooleanField("Use reduced images instead of difference", default=False)
