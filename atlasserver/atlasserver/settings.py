85991ef2fcb2ad647528cff06b0e26a1906da8dc
Luke Shingles <luke.shingles@gmail.com>
2020-10-22 15:04:32 +0100

    Avoid my.cnf

--- atlasserver/atlasserver/settings.py
+++ atlasserver/atlasserver/settings.py
@@ -83,9 +83,14 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/usr/local/etc/my.cnf',
-        },
+        # 'OPTIONS': {
+        #     # 'read_default_file': '/usr/local/etc/my.cnf',
+        # },
+        'NAME': os.environ.get('DJANGO_MYSQL_DBNAME'),
+        'USER': os.environ.get('DJANGO_MYSQL_USER'),
+        'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD'),
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
