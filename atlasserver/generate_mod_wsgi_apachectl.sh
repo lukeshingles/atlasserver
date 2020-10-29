3731cc7d3fc52693ea58f75315916e1c29179ef7
Ken Smith <k.w.smith@qub.ac.uk>
2020-10-29 17:12:25 +0000

    Updated requirements.txt

---
+++ atlasserver/generate_mod_wsgi_apachectl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create a setup script and immediately start the apache instance.  Our URL prefix
+# is specified by the --mount-point setting.  We need to specify a PYTHONPATH before
+# starting the apache instance. Run this script from THIS directory.
+mkdir -p /tmp/atlasforced
+mod_wsgi-express setup-server --working-directory atlasserver --url-alias /sne/atlasforced/static static --application-type module atlasserver.wsgi --server-root /tmp/atlasforced --port 8086 --mount-point /sne/atlasforced
+
+export PYTHONPATH=$(pwd)
+/tmp/atlasforced/apachectl start
