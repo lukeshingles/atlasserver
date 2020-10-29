54f927dd1a2bb952cb05b7ededccd9f3064f0e48
Ken Smith <k.w.smith@qub.ac.uk>
2020-10-29 17:18:59 +0000

    Modified STATIC_URL for mod_wsgi-express.  Added STATIC_ROOT.

--- atlasserver/atlasserver/settings.py
+++ atlasserver/atlasserver/settings.py
@@ -132,7 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = '/sne/atlasforced/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
