18c6e76d0bdb417fcfb22fcc1cd969f93cd20fdd
Luke Shingles <luke.shingles@gmail.com>
2020-11-03 16:02:38 +0000

    Update views.py

--- forcephot/views.py
+++ forcephot/views.py
@@ -123,7 +123,7 @@ class ForcePhotTaskViewSet(viewsets.ModelViewSet):
             return self.list(request, *args, **kwargs)
             # return redirect(reverse('task-list'), status=status.HTTP_201_CREATED, headers=headers)
         else:
-            serializer = self.get_serializer(data=request.data, many=True)
+            serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
