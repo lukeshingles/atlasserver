7f2b4ded1812c0234b06d79abbe6cb9ff3175bb6
Luke Shingles <luke.shingles@gmail.com>
2020-12-02 09:47:00 +0000

    Stop Apache server if already running

--- generate_mod_wsgi_apachectl.sh
+++ generate_mod_wsgi_apachectl.sh
@@ -2,8 +2,17 @@
 # Create a setup script and immediately start the apache instance.  Our URL prefix
 # is specified by the --mount-point setting.  We need to specify a PYTHONPATH before
 # starting the apache instance. Run this script from THIS directory.
-mkdir -p /tmp/atlasforced
-mod_wsgi-express setup-server --working-directory atlasserver --url-alias /forcedphot/static static --application-type module atlasserver.wsgi --server-root /tmp/atlasforced --port 8086 --mount-point /forcedphot --include-file httpconf.txt
+APACHEPATH="/tmp/atlasforced"
+
+if [ -f $APACHEPATH/apachectl ]; then
+    echo "Stopping Apache if already running"
+    $APACHEPATH/apachectl stop
+else
+    echo "Creating directory $APACHEPATH"
+    mkdir -p APACHEPATH
+fi
+
+mod_wsgi-express setup-server --working-directory atlasserver --url-alias /forcedphot/static static --application-type module atlasserver.wsgi --server-root $APACHEPATH --port 8086 --mount-point /forcedphot --include-file httpconf.txt
 
 export PYTHONPATH=$(pwd)
-/tmp/atlasforced/apachectl start
+$APACHEPATH/apachectl start
