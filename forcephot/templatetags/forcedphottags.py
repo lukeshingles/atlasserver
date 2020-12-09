64791ac55bde5f56cd0c3926076424b5e4dfa684
Luke Shingles <luke.shingles@gmail.com>
2020-12-09 18:31:45 +0000

    Update forcedphottags.py

--- forcephot/templatetags/forcedphottags.py
+++ forcephot/templatetags/forcedphottags.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.filter
 def tasktimesince(value):
     seconds = (datetime.datetime.now(datetime.timezone.utc) - value).total_seconds()
-    if seconds < 3001:
+    if seconds < 350:
         return f'{seconds:.0f} seconds ago'
 
     return humanize.naturaltime(value)
