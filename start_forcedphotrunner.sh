1f4208b9ca3821c7d5c0eeffa45ab36564885226
Luke Shingles <luke.shingles@gmail.com>
2020-12-17 13:59:43 +0000

    Auto-restart the task runner if it crashes

---
+++ start_forcedphotrunner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while (true) do
+   echo "Starting forcephotrunner.py" | tee -a fprunnerlog.txt
+   # your command goes here instead of sleep
+   python3 -u forcephotrunner.py > >(tee -a fprunnerlog.txt) 2> >(tee -a fprunnerlogstderr.txt >&2) && break;
+   # show result
+   exitcode=$?
+   echo "Runner crashed? Exit code was $exitcode. Restarting in two seconds..." | tee -a fprunnerlogstderr.txt
+   sleep 2
+done
--- startforcephotrunner.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 -u forcephotrunner.py > >(tee -a fprunnerlog.txt) 2> >(tee -a fprunnerlogstderr.txt >&2)
