fdbc402511dc6ac65506e14add09f5c007a64376
lukeshingles <luke.shingles@gmail.com>
2021-02-16 17:51:40 +0000

    Avoid double logging

--- start_forcedphotrunner.sh
+++ start_forcedphotrunner.sh
@@ -3,7 +3,7 @@
 while (true) do
    echo $(date) "Starting forcephotrunner.py" | tee -a fprunnerlog.txt
    # your command goes here instead of sleep
-   python3 -u forcephotrunner.py > >(tee -a fprunnerlog.txt) 2> >(tee -a fprunnerlogstderr.txt >&2) && break;
+   python3 -u forcephotrunner.py 2> >(tee -a fprunnerlogstderr.txt >&2) && break;
    # show result
    exitcode=$?
    echo $(date) " forcephotrunner.py crashed? Exit code was $exitcode. Restarting in two seconds..." | tee -a fprunnerlogstderr.txt
