b0e6d9bf7349b6ebd4fa7494934c8143fb04ab6d
lukeshingles <luke.shingles@gmail.com>
2021-02-16 16:27:04 +0000

    Update start_forcedphotrunner.sh

--- start_forcedphotrunner.sh
+++ start_forcedphotrunner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 while (true) do
-   echo "Starting forcephotrunner.py" | tee -a fprunnerlog.txt
+   echo $(date) "Starting forcephotrunner.py" | tee -a fprunnerlog.txt
    # your command goes here instead of sleep
    python3 -u forcephotrunner.py > >(tee -a fprunnerlog.txt) 2> >(tee -a fprunnerlogstderr.txt >&2) && break;
    # show result
    exitcode=$?
-   echo $(date) " Runner crashed? Exit code was $exitcode. Restarting in two seconds..." | tee -a fprunnerlogstderr.txt
+   echo $(date) " forcephotrunner.py crashed? Exit code was $exitcode. Restarting in two seconds..." | tee -a fprunnerlogstderr.txt
    sleep 2
 done
