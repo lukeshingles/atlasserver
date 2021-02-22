d9178a24487fc8f2b21b133c0e1747fdc3b7d299
lukeshingles <luke.shingles@gmail.com>
2021-02-22 15:59:27 +0000

    Sort countries by task_count, country_code

--- forcephot/views.py
+++ forcephot/views.py
@@ -318,7 +318,7 @@ def stats(request):
         dictparams['lastfinishtime'] = 'N/A'
 
     countrylist = thirtydaytasks.values_list('country_code').annotate(
-        task_count=Count('country_code')).order_by('-task_count')
+        task_count=Count('country_code')).order_by('-task_count', 'country_code')
 
     dictparams['countrylist'] = [
         (country_code_to_name(code), count) for code, count in countrylist if count > 0 and code != 'XX'][:15]
