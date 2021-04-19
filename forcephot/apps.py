5ac8366438bb625175fa7965f91ea37e609ae266
lukeshingles <luke.shingles@gmail.com>
2021-04-19 19:52:41 +0100

    Update apps.py

--- forcephot/apps.py
+++ forcephot/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class ForcephotConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'forcephot'
     verbose_name = 'ATLAS Forced Photometry Interface'
