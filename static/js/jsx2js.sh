0a8904fca8ff4a339a76e0fae7c04cd546407e64
Luke Shingles <luke.shingles@gmail.com>
2021-05-31 10:23:05 +0100

    Minify plotly script

--- static/js/jsx2js.sh
+++ static/js/jsx2js.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 babel --presets /usr/local/lib/node_modules/babel-preset-react tasklist.jsx |
 minify --js > tasklist.min.js
+#minify plotly-latest.kws.js > plotly-latest.kws.min.js
