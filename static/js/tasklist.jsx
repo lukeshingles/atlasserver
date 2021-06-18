44197f8576cdd5beae1fa4a3a6304de56ecd8014
Luke Shingles <luke.shingles@gmail.com>
2021-06-18 09:52:52 +0100

    Enable open in new tab

--- static/js/tasklist.jsx
+++ static/js/tasklist.jsx
@@ -136,10 +136,10 @@ class Task extends React.Component {
       </div>
     ];
 
-    taskbox.push(<div key="tasknum"><a key="tasklink" onClick={() => {this.props.setSingleTaskView(task.id, task.url)}}>Task {task.id}</a></div>);
+    taskbox.push(<div key="tasknum"><a key="tasklink" href={task.url} onClick={(e) => {e.preventDefault(); this.props.setSingleTaskView(task.id, task.url)}}>Task {task.id}</a></div>);
 
     if (task.parent_task_url) {
-      taskbox.push(<p key="imgrequest">Image request for <a key="parent_task_link" onClick={() => {this.props.setSingleTaskView(task.parent_task_id, task.parent_task_url)}}>Task {task.parent_task_id}</a></p>);
+      taskbox.push(<p key="imgrequest">Image request for <a key="parent_task_link" href={task.parent_task_url} onClick={(e) => {e.preventDefault(); this.props.setSingleTaskView(task.parent_task_id, task.parent_task_url)}}>Task {task.parent_task_id}</a></p>);
     } else if (task.parent_task_id) {
       taskbox.push(<p key="imgrequest">Image request for Task {task.parent_task_id} (deleted)</p>);
     }
