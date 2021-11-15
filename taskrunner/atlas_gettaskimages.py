051f9ee2d174825f076d962007960323672ad68a
Luke Shingles <luke.shingles@gmail.com>
2021-11-15 23:48:12 +0000

    Reduce image request parallel jobs from 32 to 16

--- taskrunner/atlas_gettaskimages.py
+++ taskrunner/atlas_gettaskimages.py
@@ -4,72 +4,72 @@ This script is to be run on sc01. A job datafile is converted to a zip of fits i
 """
 
 import os
-import math
+# import math
 import pandas as pd
 from pathlib import Path
 import shutil
-import subprocess
+# import subprocess
 import sys
 import tempfile
 
 
-def mjd_to_date(mjd):
-    """
-    modified from https://gist.github.com/jiffyclub/1294443
-    Convert Modified Julian Day to date.
-
-    Algorithm from 'Practical Astronomy with your Calculator or Spreadsheet',
-        4th ed., Duffet-Smith and Zwart, 2011.
-
-    Parameters
-    ----------
-    mjd : float
-        Modified Julian Day
-
-    Returns
-    -------
-    year : int
-        Year as integer. Years preceding 1 A.D. should be 0 or negative.
-        The year before 1 A.D. is 0, 10 B.C. is year -9.
-
-    month : int
-        Month as integer, Jan = 1, Feb. = 2, etc.
-
-    day : float
-        Day, may contain fractional part.
-
-    """
-    jd = mjd + 2400000.5
-    jd = jd + 0.5
-
-    F, I = math.modf(jd)
-    I = int(I)
-
-    A = math.trunc((I - 1867216.25)/36524.25)
-
-    if I > 2299160:
-        B = I + 1 + A - math.trunc(A / 4.)
-    else:
-        B = I
-
-    C = B + 1524
-    D = math.trunc((C - 122.1) / 365.25)
-    E = math.trunc(365.25 * D)
-    G = math.trunc((C - E) / 30.6001)
-
-    day = C - E + F - math.trunc(30.6001 * G)
-
-    if G < 13.5:
-        month = G - 1
-    else:
-        month = G - 13
-
-    if month > 2.5:
-        year = D - 4716
-    else:
-        year = D - 4715
-
-    return f'{year:04d}-{month:02d}-{int(day):02d}'
+# def mjd_to_date(mjd):
+#     """
+#     modified from https://gist.github.com/jiffyclub/1294443
+#     Convert Modified Julian Day to date.
+#
+#     Algorithm from 'Practical Astronomy with your Calculator or Spreadsheet',
+#         4th ed., Duffet-Smith and Zwart, 2011.
+#
+#     Parameters
+#     ----------
+#     mjd : float
+#         Modified Julian Day
+#
+#     Returns
+#     -------
+#     year : int
+#         Year as integer. Years preceding 1 A.D. should be 0 or negative.
+#         The year before 1 A.D. is 0, 10 B.C. is year -9.
+#
+#     month : int
+#         Month as integer, Jan = 1, Feb. = 2, etc.
+#
+#     day : float
+#         Day, may contain fractional part.
+#
+#     """
+#     jd = mjd + 2400000.5
+#     jd = jd + 0.5
+#
+#     F, I = math.modf(jd)
+#     I = int(I)
+#
+#     A = math.trunc((I - 1867216.25)/36524.25)
+#
+#     if I > 2299160:
+#         B = I + 1 + A - math.trunc(A / 4.)
+#     else:
+#         B = I
+#
+#     C = B + 1524
+#     D = math.trunc((C - 122.1) / 365.25)
+#     E = math.trunc(365.25 * D)
+#     G = math.trunc((C - E) / 30.6001)
+#
+#     day = C - E + F - math.trunc(30.6001 * G)
+#
+#     if G < 13.5:
+#         month = G - 1
+#     else:
+#         month = G - 13
+#
+#     if month > 2.5:
+#         year = D - 4716
+#     else:
+#         year = D - 4715
+#
+#     return f'{year:04d}-{month:02d}-{int(day):02d}'
 
 
 def main():
@@ -141,7 +141,7 @@ def main():
     #     with wpdatefile.open('w') as f:
     #         f.writelines(wpdatelines)
 
-    os.system(f'parallel --jobs 32 < {commandfile}')
+    os.system(f'parallel --jobs 16 < {commandfile}')
 
     for firstfitsoutpath in [firstfitsoutpath_c, firstfitsoutpath_o]:
         if firstfitsoutpath is not None:
