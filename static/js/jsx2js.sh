a99d003d84a14db1fc321e07bff8dee2e601328d
Luke Shingles <luke.shingles@gmail.com>
2022-07-22 19:48:16 +0100

    tasklist.jsx: Use version in file name to avoid caching issues

--- static/js/jsx2js.sh
+++ static/js/jsx2js.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 babel --presets /opt/homebrew/lib/node_modules/babel-preset-react tasklist.jsx |
-minify --js > tasklist.min.js
+minify --js > tasklist.v2.min.js
 #minify plotly-latest.kws.js > plotly-latest.kws.min.js
