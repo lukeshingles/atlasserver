5ed7af79b9ca1e9b6f81a1b8a4cf867962ab5054
Luke Shingles <luke.shingles@gmail.com>
2022-09-02 13:56:48 +0100

    Fix linter errors

--- update_geoipdatabase.sh
+++ update_geoipdatabase.sh
@@ -2,7 +2,7 @@
 
 source .env
 
-ATLASSERVERPATH=$(dirname $(realpath "$0"))
+ATLASSERVERPATH="$(dirname "$(realpath "$0")")"
 
 curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz" --output "/tmp/GeoLite2-ASN.tar.gz"
 tar  -zxvf /tmp/GeoLite2-ASN.tar.gz --to-stdout GeoLite2-ASN_*/GeoLite2-ASN.mmdb > "$ATLASSERVERPATH/atlasserver/GeoLite2-ASN.mmdb"
