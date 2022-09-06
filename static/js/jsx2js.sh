043ed7c0331590197e03bd691a51c9354642b593
Luke Shingles <luke.shingles@gmail.com>
2022-09-06 16:21:41 +0100

    Update js

--- static/js/jsx2js.sh
+++ static/js/jsx2js.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 babel --minified --presets /opt/homebrew/lib/node_modules/babel-preset-react newrequest.jsx task.jsx tasklist.jsx |
-minify --js > tasklist.20220811.min.js
+minify --js > tasklist.min.js
