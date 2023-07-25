568df990aabad2db56dc346ec4814b11ad83ae3d
Luke Shingles <luke.shingles@gmail.com>
2023-07-25 11:09:58 +0100

    Update exception.py

--- atlasserver/forcephot/exception.py
+++ atlasserver/forcephot/exception.py
@@ -7,19 +7,17 @@ from rest_framework.views import exception_handler
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    # Now add the HTTP status code to the response.
-
-    if response is not None and response.status_code in [401, 403]:
-        try:
-            # login_url = request.build_absolute_uri(reverse('rest_framework:login'))
-            login_url = reverse("rest_framework:login")
-            request = context.get("request")
-            next = escape(request.path)
-            redirect_url = f"{login_url}?next={next}"
-
-        except NoReverseMatch:
-            redirect_url = "/"
-
-        return HttpResponseRedirect(redirect_url)
-    else:
+    if response is None or response.status_code not in [401, 403]:
         return response
+
+    try:
+        # login_url = request.build_absolute_uri(reverse('rest_framework:login'))
+        login_url = reverse("rest_framework:login")
+        request = context.get("request")
+        next = escape(request.path)
+        redirect_url = f"{login_url}?next={next}"
+
+    except NoReverseMatch:
+        redirect_url = "/"
+
+    return HttpResponseRedirect(redirect_url)
