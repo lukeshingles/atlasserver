6197dd02eea88f33234c219d78eb4b114950d202
Luke Shingles <luke.shingles@gmail.com>
2023-07-26 10:31:55 +0100

    Revert "Add npm_install_babel_minify.sh and update to @babel/cli@7"
    This reverts commit c1492768edd2a5aba0f9447249c56ca0e375cc39.

--- static/js/jsx2js.sh
+++ static/js/jsx2js.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
-babel --minified --presets $(npm -g root)/@babel/preset-react newrequest.jsx task.jsx tasklist.jsx | minify --js > tasklist.min.js
+babel --minified --presets /opt/homebrew/lib/node_modules/babel-preset-react newrequest.jsx task.jsx tasklist.jsx |
+minify --js > tasklist.min.js
--- static/js/npm_install_babel_minify.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-npm install --global @babel/cli
-npm install --global @babel/preset-react
-npm install --global minify
