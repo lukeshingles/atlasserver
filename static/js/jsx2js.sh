c1492768edd2a5aba0f9447249c56ca0e375cc39
Luke Shingles <luke.shingles@gmail.com>
2023-07-26 10:30:44 +0100

    Add npm_install_babel_minify.sh and update to @babel/cli@7

--- static/js/jsx2js.sh
+++ static/js/jsx2js.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env zsh
 
-babel --minified --presets /opt/homebrew/lib/node_modules/babel-preset-react newrequest.jsx task.jsx tasklist.jsx |
-minify --js > tasklist.min.js
+babel --minified --presets $(npm -g root)/@babel/preset-react newrequest.jsx task.jsx tasklist.jsx | minify --js > tasklist.min.js
