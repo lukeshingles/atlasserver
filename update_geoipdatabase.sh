895048412c8122e3ff97d3927640e3d3c5044f34
Luke Shingles <luke.shingles@gmail.com>
2024-05-07 15:06:32 +0300

    Update update_geoipdatabase.sh

--- update_geoipdatabase.sh
+++ update_geoipdatabase.sh
@@ -6,11 +6,14 @@ fi
 
 ATLASSERVERPATH="$(dirname "$(realpath "$0")")"
 
-curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz" --output "/tmp/GeoLite2-ASN.tar.gz"
-tar  -zxvf /tmp/GeoLite2-ASN.tar.gz --to-stdout GeoLite2-ASN_*/GeoLite2-ASN.mmdb > "$ATLASSERVERPATH/atlasserver/GeoLite2-ASN.mmdb"
-rm /tmp/GeoLite2-ASN.tar.gz
+
+if curl -J -L -u 504450:$MAXMIND_LICENSE_KEY "https://download.maxmind.com/geoip/databases/GeoLite2-ASN/download?suffix=tar.gz" --output "/tmp/GeoLite2-ASN.tar.gz"; then
+    tar -zxvf /tmp/GeoLite2-ASN.tar.gz --to-stdout GeoLite2-ASN_*/GeoLite2-ASN.mmdb > "$ATLASSERVERPATH/atlasserver/GeoLite2-ASN.mmdb"
+    rm /tmp/GeoLite2-ASN.tar.gz
+fi
 
 
-curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_LICENSE_KEY&suffix=tar.gz" --output "/tmp/GeoLite2-City.tar.gz"
-tar  -zxvf /tmp/GeoLite2-City.tar.gz --to-stdout "GeoLite2-City_*/GeoLite2-City.mmdb" > "$ATLASSERVERPATH/atlasserver/GeoLite2-City.mmdb"
-rm /tmp/GeoLite2-City.tar.gz
+if curl -J -L -u 504450:$MAXMIND_LICENSE_KEY "https://download.maxmind.com/geoip/databases/GeoLite2-City/download?suffix=tar.gz" --output "/tmp/GeoLite2-City.tar.gz"; then
+    tar -zxvf /tmp/GeoLite2-City.tar.gz --to-stdout "GeoLite2-City_*/GeoLite2-City.mmdb" > "$ATLASSERVERPATH/atlasserver/GeoLite2-City.mmdb"
+    rm /tmp/GeoLite2-City.tar.gz
+fi
