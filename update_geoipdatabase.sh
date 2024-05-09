6184f2a63f74bea14fe63588aec5656e3b6f248c
Luke Shingles <luke.shingles@gmail.com>
2024-05-09 15:37:18 +0300

    Update update_geoipdatabase.sh

--- update_geoipdatabase.sh
+++ update_geoipdatabase.sh
@@ -9,7 +9,7 @@ ATLASSERVERPATH="$(dirname "$(realpath "$0")")"
 tempdir=$(mktemp -d)
 if curl -J -L -u 504450:$MAXMIND_LICENSE_KEY "https://download.maxmind.com/geoip/databases/GeoLite2-ASN/download?suffix=tar.gz" --output "$tempdir/GeoLite2-ASN.tar.gz"; then
     tar -zxvf "$tempdir/GeoLite2-ASN.tar.gz" -C $tempdir
-    cp "$tempdir/GeoLite2-*/GeoLite2-ASN.mmdb" "$ATLASSERVERPATH/atlasserver/GeoLite2-ASN.mmdb"
+    cp $tempdir/GeoLite2-*/GeoLite2-ASN.mmdb "$ATLASSERVERPATH/atlasserver/GeoLite2-ASN.mmdb"
     rm -rf "$tempdir"
 fi
 
@@ -17,6 +17,6 @@ fi
 tempdir=$(mktemp -d)
 if curl -J -L -u 504450:$MAXMIND_LICENSE_KEY "https://download.maxmind.com/geoip/databases/GeoLite2-City/download?suffix=tar.gz" --output "$tempdir/GeoLite2-City.tar.gz"; then
     tar -zxvf "$tempdir/GeoLite2-City.tar.gz" -C "$tempdir"
-    cp "$tempdir/GeoLite2-*/GeoLite2-City.mmdb" "$ATLASSERVERPATH/atlasserver/GeoLite2-City.mmdb"
+    cp $tempdir/GeoLite2-*/GeoLite2-City.mmdb "$ATLASSERVERPATH/atlasserver/GeoLite2-City.mmdb"
     rm -rf "$tempdir"
 fi
