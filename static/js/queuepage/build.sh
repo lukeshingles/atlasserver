4c0d396ea6d9f7968f8584168ff53640ed09ffd5
Luke Shingles <luke.shingles@gmail.com>
2025-01-31 08:45:49 +0000

    Update npm deps

--- static/js/queuepage/build.sh
+++ static/js/queuepage/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-babel --minified --presets @babel/preset-react src/newrequest.jsx > ../newrequest.min.js
-babel --minified --presets @babel/preset-react src/tasklist.jsx > ../tasklist.min.js
+npx babel --minified --presets @babel/preset-react src/newrequest.jsx > ../newrequest.min.js
+npx babel --minified --presets @babel/preset-react src/tasklist.jsx > ../tasklist.min.js
 
-babel --minified --presets @babel/preset-react src/lightcurveplotly.js > ../lightcurveplotly.min.js
+npx babel --minified --presets @babel/preset-react src/lightcurveplotly.js > ../lightcurveplotly.min.js
