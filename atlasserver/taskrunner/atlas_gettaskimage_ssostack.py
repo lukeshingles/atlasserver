61ece3ae8d78f8f9f698ea59b6f186d20d4ce2eb
Ken Smith <k.w.smith@qub.ac.uk>
2026-06-04 18:22:56 +0100

    Added new python script and monsta script to convert the solar system object stacked image to jpeg.

---
+++ atlasserver/taskrunner/atlas_gettaskimage_ssostack.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Convert the solar system stack FITS file to a JPEG. This script is to be run on sc01.
+   Also depends on a new monsta script, which is not in standard ATLAS pipeline bin directory
+   (tvjpeg_ssostack.pro)"""
+
+import os
+import sys
+from pathlib import Path
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("ERROR: exactly one argument must be specified: [FITSFILE]")
+        sys.exit(1)
+        return
+
+    stackedfitsfile = sys.argv[1]
+
+    if not Path(stackedfitsfile).exists():
+        return
+
+    # Force a timeout if monsta takes longer than 5 seconds. Should be instant.
+    os.system(
+        "timeout 5 /atlas/vendor/monsta/bin/monsta ~/tvjpeg_ssostack.pro "
+        f"{stackedfitsfile} -1 10"
+        "\n"
+    )
+
+    # jobxxxxx.fits.jpg to jobxxxx.jpg
+    Path(stackedfitsfile).with_suffix(".fits.jpg").rename(Path(stackedfitsfile).with_suffix(".jpg"))
+
+
+if __name__ == "__main__":
+    main()
